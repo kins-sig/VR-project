0f8637778202a66fee0986f3cdaff6600ec72f53
kins-sig <kinsley.sigmund@gmail.com>
2020-04-21 18:18:08 -0400

    different lasers

--- SCALE/Assets/Scripts/LaserScript.cs
+++ SCALE/Assets/Scripts/LaserScript.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class LaserScript : MonoBehaviour
 {
 
-    LineRenderer line; 
+    LineRenderer scaleUpLine; 
+    LineRenderer scaleDownLine; 
+
 
     // Start is called before the first frame update
     void Start()
     {
-        line = gameObject.GetComponent<LineRenderer>();
-        line.enabled = false; 
+        scaleUpLine = GameObject.FindWithTag("ScaleUp").GetComponent<LineRenderer>();
+        scaleUpLine.enabled = false; 
+        scaleDownLine = GameObject.FindWithTag("ScaleDown").GetComponent<LineRenderer>();
+        scaleDownLine.enabled = false; 
     }
 
     // Update is called once per frame
@@ -19,31 +23,89 @@ public class LaserScript : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            StopCoroutine("FireLaser");
-            StartCoroutine("FireLaser");
+            StopCoroutine("FireLaserUp");
+            StartCoroutine("FireLaserUp");
+        }
+            
+        if (Input.GetButtonDown("Fire2"))
+        {
+            StopCoroutine("FireLaserDown");
+            StartCoroutine("FireLaserDown");
         }
     }
 
-    IEnumerator FireLaser()
+    IEnumerator FireLaserUp()
 	{
-        line.enabled = true;
+        scaleUpLine.enabled = true;
 		while (Input.GetButton("Fire1"))
 		{
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-            line.SetPosition(0, ray.origin);
+            scaleUpLine.SetPosition(0, ray.origin);
 
             if(Physics.Raycast(ray, out hit,100))
 			{
-                line.SetPosition(1, hit.point);
+                scaleUpLine.SetPosition(1, hit.point);
+                if(hit.collider.CompareTag("Scaleable")){
+                    Debug.Log("We hit a player!");
+
+                }
 
             }
 			else 
-                line.SetPosition(1, ray.GetPoint(100));
+                scaleUpLine.SetPosition(1, ray.GetPoint(100));
             
 
             yield return null;
 		}
-        line.enabled = false; 
+        scaleUpLine.enabled = false; 
 	}
+
+    IEnumerator FireLaserDown()
+	{
+        scaleDownLine.enabled = true;
+		while (Input.GetButton("Fire2"))
+		{
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            scaleDownLine.SetPosition(0, ray.origin);
+
+            if(Physics.Raycast(ray, out hit,100))
+			{
+                if(hit.collider.name == "companion cube"){
+                    Debug.Log("BAM");
+                }
+                scaleDownLine.SetPosition(1, hit.point);
+                if(hit.collider.CompareTag("Scaleable")){
+                    Debug.Log("We hit a player!");
+
+                }
+
+            }
+			else 
+                scaleDownLine.SetPosition(1, ray.GetPoint(100));
+            
+            if(hit.collider.CompareTag("Scaleable")){
+                Debug.Log("BAM");
+            }
+
+            yield return null;
+		}
+        scaleDownLine.enabled = false; 
+	}
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag ("Scalable")){
+            Debug.Log("HI");
+        }
+    }
+    // void UpScale(Vector3 scale) {
+ 
+    //      scale += new Vector3(0.1f,0.1f,0.1f);
+    //      a.transform.localScale = scale;
+    //  }
+    //  void DownScale(Vector3 scale)
+    //  {
+    //      scale -= new Vector3(0.1f, 0.1f, 0.1f);
+    //      a.transform.localScale = scale;
+    //  }
 }
